86ca13aaf005e588713e5c4fe3af5effca0c11fd
echarin <wanmingkang@gmail.com>
2023-01-27 11:32:58 +0800

    completion of calculator

--- task02/src/calculator/Constants.java
+++ task02/src/calculator/Constants.java
@@ -1,5 +1,8 @@
 package calculator;
 
 public class Constants {
-    
+    public static final String[] OPERATORS = {"+", "/", "*", "-"};
+    public static final String LAST = "$last";
+
+    private Constants() {}
 }
--- task02/src/calculator/Main.java
+++ task02/src/calculator/Main.java
@@ -7,50 +7,64 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        
 
         Console cons = System.console();
+        String input = "";
+        float result = 0.0f;
 
+        System.out.println("Welcome.");
         while (true) {
-            String input = cons.readLine("> ");
+            input = cons.readLine("> ");
+
             if (input.matches("exit")) {
-                break;
+                System.out.print("Bye bye");
+                System.exit(0);
             }
         
             input = input.trim().toLowerCase();
             String[] inputs = input.split(" ");
 
-            // Input validation
-            // Check if a x b format is followed
             if (inputs.length != 3) {
                 System.out.println("Format: operand operator operand");
                 continue;
             }
             
-            // Check if operator is valid
-            Set<String> operators = new HashSet<>(Arrays.asList("+", "/", "*", "-"));
+            Set<String> operators = new HashSet<>(Arrays.asList(Constants.OPERATORS));
             String operator = inputs[1]; 
             if (!(operators.contains(operator))) {
                 System.out.println("Incorrect operator: only accepts +, -, /, *");
                 continue;
             }
 
-            // Check if operands are floats
-            Float operand1, operand2 = 0f;
-            try {
-                operand1 = Float.parseFloat(inputs[0]);
-                operand2 = Float.parseFloat(inputs[2]);
-            } catch (NumberFormatException ex) {
-                System.out.println("Operands must be floats");
-                continue;
+            float operand1, operand2;
+            String operandStr1 = inputs[0], operandStr2 = inputs[2];
+            if (operandStr1.equalsIgnoreCase(Constants.LAST)) {
+                operand1 = result;
+            } else {
+                try {
+                    operand1 = Float.parseFloat(inputs[0]);
+                } catch (NumberFormatException ex) {
+                    System.out.println("Operands must be floats");
+                    continue;
+                }
+            }
+
+            if (operandStr2.equalsIgnoreCase(Constants.LAST)) {
+                operand2 = result;
+            } else {
+                try {
+                    operand2 = Float.parseFloat(inputs[2]);
+                } catch (NumberFormatException ex) {
+                    System.out.println("Operands must be floats");
+                    continue;
+                }
             }
 
             if (Float.isNaN(operand1) || Float.isNaN(operand2)) {
                 System.out.println("Operand was not a number");
                 continue;
             }
-
-            Float result = 0.0f;
+            
             switch (operator) {
                 case "+":
                     result = operand1 + operand2;
@@ -66,7 +80,11 @@ public class Main {
                     break;
             }
 
-            System.out.printf("> Answer: %s\n", result);
+            if (result % 1 == 0) {
+                System.out.printf("%s\n", (int) result);
+            } else {
+                System.out.printf("%s\n", result);
+            }
         }
     }
 }
